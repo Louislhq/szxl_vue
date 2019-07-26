35a514ae2894e2b7f539ff214647f0623f94bd26
Louis <519244107@qq.com>
2019-07-26 17:51:15 +0800

    更新

--- src/permission.js
+++ src/permission.js
@@ -3,6 +3,7 @@ import store from './vuex/store'
 import { getToken } from '@/utils/auth'
 
 const whiteList = ['/login']
+//全局前置守卫每次都判断用户是否已经登录，没有登录则跳到登录页
 router.beforeEach((to, from, next) => {
 
     if (getToken()) {
